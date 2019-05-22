f155b8b425051f6a8a0b3e0d8e28c99e9cf288d7
Davi Lima <davilima6@gmail.com>
2019-05-22 15:38:53 +0200

    Update import point for components

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,8 @@
  * @example import { FormContainer } from 'components';
  */
 
-export { default as FormContainer } from './FormContainer/FormContainer';
+export { default as Button } from './Button/Button';
+export { default as Field } from './Field/Field';
 export { default as Form } from './Form/Form';
+export { default as FormContainer } from './FormContainer/FormContainer';
+export { default as Spinner } from './Spinner/Spinner';
